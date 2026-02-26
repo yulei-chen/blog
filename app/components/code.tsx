e0e33171771f4c5fe1e18c873fba5195772a1a98
Yulei Chen <chenyuleiw@gmail.com>
2026-02-26 19:28:52 +0100

    reset

--- app/components/code.tsx
+++ app/components/code.tsx
@@ -1,13 +1,11 @@
 import { Pre, RawCode, highlight } from "codehike/code"
 import { callout } from "./annotations/callout"
-import { className } from "./classname"
-import { diff } from "./diff"
 export async function Code({ codeblock }: { codeblock: RawCode }) {
   const highlighted = await highlight(codeblock, "github-dark")
   return (
     <Pre
       code={highlighted}
-      handlers={[callout, className, diff]}
+      handlers={[callout]}
       className="border border-zinc-800"
     />
   )
